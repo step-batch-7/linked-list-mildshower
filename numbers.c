1c8f43b58124902dd95fce853932aa6e79d687fe
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-05-01 05:02:29 +0530

    added take_number_from_user function
    this function asks user to enter a number and returns the entered number

--- numbers.c
+++ numbers.c
@@ -2,10 +2,19 @@
 #include "list.h"
 
 void display_menu(void);
+int take_number_from_user(void);
 
 void display_menu(void)
 {
   printf("\n\nMain Menu\n---------\n(a) add a number to the end of the list\n(b) add a number to the start of the list\n(c) insert a number at a given position in the list\n(d) add a unique item on the list at the end(if it alreay exists, do not insert)\n(e) remove a number from the beginning of the list\n(f) remove a number from the end of the list\n(g) remove a number from a given position in the list\n(h) remove first occurrence of a number\n(i) remove all occurrences of a number\n(j) clear the whole list\n(k) check if a number exists in the list\n(l) display the list of numbers\n(m) exit\n\nPlease enter the alphabet of the operation you would like to perform\n");
+}
+
+int take_number_from_user(void)
+{
+  int given_number;
+  printf("\nEnter the number: ");
+  scanf("%d", &given_number);
+  return given_number;
 }
 
 int main(void)
@@ -24,12 +33,11 @@ int main(void)
     switch (chosen_option)
     {
     case 'a':
-      printf("\nEnter the number: ");
-      int given_number;
-      scanf("%d", &given_number);
-      Status addition_status = add_to_end(list, given_number);
-      printf("%d is %s added.", given_number, addition_status ? "" : "not");
+    {
+      Status addition_status = add_to_end(list, take_number_from_user());
+      printf("Addition was %ssuccessful.", addition_status ? "" : "un");
       break;
+    }
 
     case 'l':
       display(list);
