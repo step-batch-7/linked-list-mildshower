eeeb4d587f0b1ee35dc284a9b83bc1aa9e262c5f
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-05-01 04:14:41 +0530

    added display_menu function
    this function displays the whole menu to the user

--- numbers.c
+++ numbers.c
@@ -1,4 +1,14 @@
+#include <stdio.h>
+
+void display_menu(void);
+
+void display_menu(void)
+{
+  printf("\n\nMain Menu\n---------\n(a) add a number to the end of the list\n(b) add a number to the start of the list\n(c) insert a number at a given position in the list\n(d) add a unique item on the list at the end(if it alreay exists, do not insert)\n(e) remove a number from the beginning of the list\n(f) remove a number from the end of the list\n(g) remove a number from a given position in the list\n(h) remove first occurrence of a number\n(i) remove all occurrences of a number\n(j) clear the whole list\n(k) check if a number exists in the list\n(l) display the list of numbers\n(m) exit\n\nPlease enter the alphabet of the operation you would like to perform\n");
+}
+
 int main(void)
 {
+  display_menu();
   return 0;
 }
