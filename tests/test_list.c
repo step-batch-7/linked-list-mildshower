b1a81113f368a4fa504cae6df0cc77e035655266
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-05-05 15:50:15 +0530

    increased reliability of code with more tests

--- tests/test_list.c
+++ tests/test_list.c
@@ -4,16 +4,44 @@
 
 void test_add_to_end(void)
 {
-  printf("Should add new element at end\n");
+  printf("\n\nTesting add_to_end\n\n");
+
   List_ptr list = create_list();
+  printf("\tShould add new element at End when list is empty\n");
   assert(add_to_end(list, 0));
+  assert(list->last->value == 0);
   assert(list->count == 1);
+  printf("\t\t--passed\n\n");
+
+  printf("\tShould add new element at End when list has elements\n");
+  assert(add_to_end(list, 1));
+  assert(list->last->value == 1);
+  assert(list->count == 2);
+  printf("\t\t--passed\n\n");
+}
+
+void test_add_to_start(void)
+{
+  printf("\n\nTesting add_to_start\n\n");
+
+  List_ptr list = create_list();
+  printf("\tShould add new element at Start when list is empty\n");
+  assert(add_to_start(list, 0));
+  assert(list->head->value == 0);
+  assert(list->count == 1);
+  printf("\t\t--passed\n\n");
+
+  printf("\tShould add new element at Start when list has elements\n");
+  assert(add_to_start(list, 1));
+  assert(list->head->value == 1);
+  assert(list->count == 2);
   printf("\t\t--passed\n\n");
 }
 
 int main(void)
 {
   test_add_to_end();
+  test_add_to_start();
 
   return 0;
 }
