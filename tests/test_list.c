9a3a0b4964a30b8607eab40710660885691b7bd5
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-05-05 16:06:56 +0530

    added tests for add_unique

--- tests/test_list.c
+++ tests/test_list.c
@@ -7,13 +7,13 @@ void test_add_to_end(void)
   printf("\n\nTesting add_to_end\n\n");
 
   List_ptr list = create_list();
-  printf("\tShould add new element at End when list is empty\n");
+  printf("\tShould add new number at End when list is empty\n");
   assert(add_to_end(list, 0));
   assert(list->last->value == 0);
   assert(list->count == 1);
   printf("\t\t--passed\n\n");
 
-  printf("\tShould add new element at End when list has elements\n");
+  printf("\tShould add new number at End when list has elements\n");
   assert(add_to_end(list, 1));
   assert(list->last->value == 1);
   assert(list->count == 2);
@@ -25,13 +25,13 @@ void test_add_to_start(void)
   printf("\n\nTesting add_to_start\n\n");
 
   List_ptr list = create_list();
-  printf("\tShould add new element at Start when list is empty\n");
+  printf("\tShould add new number at Start when list is empty\n");
   assert(add_to_start(list, 0));
   assert(list->head->value == 0);
   assert(list->count == 1);
   printf("\t\t--passed\n\n");
 
-  printf("\tShould add new element at Start when list has elements\n");
+  printf("\tShould add new number at Start when list has elements\n");
   assert(add_to_start(list, 1));
   assert(list->head->value == 1);
   assert(list->count == 2);
@@ -43,22 +43,42 @@ void test_insert_at(void)
   printf("\n\nTesting insert_at\n\n");
 
   List_ptr list = create_list();
-  printf("\tShould add new element at the given valid position\n");
+  printf("\tShould add new number at the given valid position\n");
   assert(insert_at(list, 0, 0));
   assert(list->head->value == 0);
   assert(list->last->value == 0);
   assert(list->count == 1);
   printf("\t\t--passed\n\n");
 
-  printf("\tShould not add new element a position is given that exceeds allowed limit\n");
+  printf("\tShould not add new number a position is given that exceeds allowed limit\n");
   assert(!insert_at(list, 0, 10));
   assert(list->head->value == 0);
   assert(list->last->value == 0);
   assert(list->count == 1);
   printf("\t\t--passed\n\n");
 
-  printf("\tShould not add new element a negative position is given\n");
+  printf("\tShould not add new number a negative position is given\n");
   assert(!insert_at(list, 0, 10));
+  assert(list->head->value == 0);
+  assert(list->last->value == 0);
+  assert(list->count == 1);
+  printf("\t\t--passed\n\n");
+}
+
+void test_add_unique(void)
+{
+  printf("\n\nTesting add_unique\n\n");
+
+  List_ptr list = create_list();
+  printf("\tShould add new number if the number is not present in the list\n");
+  assert(add_unique(list, 0));
+  assert(list->head->value == 0);
+  assert(list->last->value == 0);
+  assert(list->count == 1);
+  printf("\t\t--passed\n\n");
+
+  printf("\tShould not add new number if the number is present in the list\n");
+  assert(!add_unique(list, 0));
   assert(list->head->value == 0);
   assert(list->last->value == 0);
   assert(list->count == 1);
@@ -70,6 +90,7 @@ int main(void)
   test_add_to_end();
   test_add_to_start();
   test_insert_at();
+  test_add_unique();
 
   return 0;
 }
