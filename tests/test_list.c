93989052c792c29d34f483c1145ba8143c60e58b
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-05-05 15:56:59 +0530

    added tests for insert_at

--- tests/test_list.c
+++ tests/test_list.c
@@ -38,10 +38,38 @@ void test_add_to_start(void)
   printf("\t\t--passed\n\n");
 }
 
+void test_insert_at(void)
+{
+  printf("\n\nTesting insert_at\n\n");
+
+  List_ptr list = create_list();
+  printf("\tShould add new element at the given valid position\n");
+  assert(insert_at(list, 0, 0));
+  assert(list->head->value == 0);
+  assert(list->last->value == 0);
+  assert(list->count == 1);
+  printf("\t\t--passed\n\n");
+
+  printf("\tShould not add new element a position is given that exceeds allowed limit\n");
+  assert(!insert_at(list, 0, 10));
+  assert(list->head->value == 0);
+  assert(list->last->value == 0);
+  assert(list->count == 1);
+  printf("\t\t--passed\n\n");
+
+  printf("\tShould not add new element a negative position is given\n");
+  assert(!insert_at(list, 0, 10));
+  assert(list->head->value == 0);
+  assert(list->last->value == 0);
+  assert(list->count == 1);
+  printf("\t\t--passed\n\n");
+}
+
 int main(void)
 {
   test_add_to_end();
   test_add_to_start();
+  test_insert_at();
 
   return 0;
 }
